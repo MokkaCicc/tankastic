5fc4eaf9b08fb528961cb50165cf7c3d03e6f5d0
Naoi18 <44975529+Naoi18@users.noreply.github.com>
2022-02-07 18:28:29 +0100

    added link in home page, changed link color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import { PrismaClient, Tank } from '@prisma/client'
+import Link from 'next/link'
 import Map from '../components/map'
 
 export default function Home(props: { tanks: Tank[] }) {
 	return (
-		<Map cols={20} rows={15} tanks={props.tanks} />
+		<div>
+			<Link href="/account/register">
+				Register
+			</Link>
+			<Link href="/account/login">
+				Login
+			</Link>
+
+			<Map cols={20} rows={15} tanks={props.tanks} />
+		</div>
 	)
 }
 
